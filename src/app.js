998cdbd5ea0a5a431382ea775a8bd6676c8442e3
Ocaanaaa <166707320+Ocaanaaa@users.noreply.github.com>
2024-06-28 18:53:32 +0000

    [Random card generator]

--- src/app.js
+++ src/app.js
@@ -9,3 +9,39 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+window.onload = function() {
+  const ranks = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const suits = ["&hearts;", "&diams;", "&clubs;", "&spades;"];
+
+  function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  const randomRank = getRandomElement(ranks);
+  const randomSuit = getRandomElement(suits);
+
+  document
+    .querySelectorAll(".rank")
+    .forEach(element => (element.innerHTML = randomRank));
+  document.querySelectorAll(".suit").forEach(element => {
+    element.innerHTML = randomSuit;
+
+    if (randomSuit == "&hearts;" || randomSuit == "&diams;") {
+      element.style.color = "red";
+    }
+  });
+};
